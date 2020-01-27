99df33a13d7f0900582a3514ab8fa35e4b6b6d19
ilona <ilona.pradel09@gmail.com>
2020-01-27 09:58:13 +0100

    Amélioration page de recherche : display étoiles et value du slider

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -9,4 +9,30 @@ export class SearchPage implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  star = "star";
+  star_out = "star-outline";
+  stars_name: Array<string> = [
+    "star",
+    "star-outline",
+    "star-outline",
+    "star-outline",
+    "star-outline"
+  ];
+
+  rangeValue: { lower: number; upper: number } = { lower: 20, upper: 100 };
+
+  clickOnStar(position: number) {
+    for (let number of [0, 1, 2, 3, 4]) {
+      if (position > number) {
+        this.stars_name[number] = "star";
+      } else {
+        this.stars_name[number] = "star-outline";
+      }
+    }
+  }
+
+  changeValue(value: { lower: number; upper: number }) {
+    this.rangeValue = value;
+  }
 }
