78e333018dc44752c5a438b505b65b34cf815dda
ilona <ilona.pradel09@gmail.com>
2020-01-29 17:38:35 +0100

    Suppression de la page de test de la classe utilitaire

--- src/app/utilities-class.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UtilitiesClass } from './utilities-class';
-
-describe('UtilitiesClass', () => {
-  it('should create an instance', () => {
-    expect(new UtilitiesClass()).toBeTruthy();
-  });
-});
