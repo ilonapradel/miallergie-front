410ddaa55b18e1a47534a49e2553122aced9b76e
ilona <ilona.pradel09@gmail.com>
2020-04-17 16:40:38 +0200

    Debut change to tab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,107 +5,112 @@ const routes: Routes = [
   { path: "", redirectTo: "/authentication", pathMatch: "full" },
   {
     path: "home",
-    loadChildren: () => import("./home/home.module").then(m => m.HomePageModule)
+    loadChildren: () =>
+      import("./home/home.module").then((m) => m.HomePageModule),
   },
   {
     path: "authentication",
     loadChildren: () =>
       import("./authentication/authentication.module").then(
-        m => m.AuthenticationPageModule
-      )
+        (m) => m.AuthenticationPageModule
+      ),
   },
   {
     path: "profile",
     loadChildren: () =>
-      import("./profile/profile.module").then(m => m.ProfilePageModule)
+      import("./profile/profile.module").then((m) => m.ProfilePageModule),
   },
   {
     path: "search",
     loadChildren: () =>
-      import("./search/search.module").then(m => m.SearchPageModule)
+      import("./search/search.module").then((m) => m.SearchPageModule),
   },
   {
     path: "register",
     loadChildren: () =>
-      import("./register/register.module").then(m => m.RegisterPageModule)
+      import("./register/register.module").then((m) => m.RegisterPageModule),
   },
   {
     path: "recipe-display",
     loadChildren: () =>
       import("./recipe-display/recipe-display.module").then(
-        m => m.RecipeDisplayPageModule
-      )
+        (m) => m.RecipeDisplayPageModule
+      ),
   },
   {
     path: "change-username",
     loadChildren: () =>
       import("./change-username/change-username.module").then(
-        m => m.ChangeUsernamePageModule
-      )
+        (m) => m.ChangeUsernamePageModule
+      ),
   },
   {
     path: "change-password",
     loadChildren: () =>
       import("./change-password/change-password.module").then(
-        m => m.ChangePasswordPageModule
-      )
+        (m) => m.ChangePasswordPageModule
+      ),
   },
   {
     path: "change-email",
     loadChildren: () =>
       import("./change-email/change-email.module").then(
-        m => m.ChangeEmailPageModule
-      )
+        (m) => m.ChangeEmailPageModule
+      ),
   },
   {
     path: "change-preferences",
     loadChildren: () =>
       import("./change-preferences/change-preferences.module").then(
-        m => m.ChangePreferencesPageModule
-      )
+        (m) => m.ChangePreferencesPageModule
+      ),
   },
   {
     path: "add-recipe",
     loadChildren: () =>
-      import("./add-recipe/add-recipe.module").then(m => m.AddRecipePageModule)
+      import("./add-recipe/add-recipe.module").then(
+        (m) => m.AddRecipePageModule
+      ),
   },
   {
     path: "friend",
     loadChildren: () =>
-      import("./friend/friend.module").then(m => m.FriendPageModule)
+      import("./friend/friend.module").then((m) => m.FriendPageModule),
   },
   {
     path: "add-friend",
     loadChildren: () =>
-      import("./add-friend/add-friend.module").then(m => m.AddFriendPageModule)
+      import("./add-friend/add-friend.module").then(
+        (m) => m.AddFriendPageModule
+      ),
   },
   {
     path: "update-friend",
     loadChildren: () =>
       import("./update-friend/update-friend.module").then(
-        m => m.UpdateFriendPageModule
-      )
+        (m) => m.UpdateFriendPageModule
+      ),
   },
   {
     path: "user-recipes",
     loadChildren: () =>
       import("./user-recipes/user-recipes.module").then(
-        m => m.UserRecipesPageModule
-      )
+        (m) => m.UserRecipesPageModule
+      ),
   },
   {
     path: "edit-recipe",
     loadChildren: () =>
       import("./edit-recipe/edit-recipe.module").then(
-        m => m.EditRecipePageModule
-      )
-  }
+        (m) => m.EditRecipePageModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { NavController } from "@ionic/angular";
+import { Routes } from "@angular/router";
 
 @Component({
   selector: "app-menu",
   templateUrl: "./menu.component.html",
-  styleUrls: ["./menu.component.scss"]
+  styleUrls: ["./menu.component.scss"],
 })
 export class MenuComponent implements OnInit {
   constructor(public navCtrl: NavController) {}
